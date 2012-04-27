20d0ad31e5e3ad609e741b820d2e767341da2df1
FUKUI Osamu <ofukui@irubysystems.com>
2012-04-27 20:15:43 +0900

    takegakiアプリ作成開始

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tategaki::Application.config.secret_token = '0be5700401b2c51b4c58904455e07da930a1d54eb950fe92ae74059f8d6c09becb62e4bb64eeede51a540cb801f1847ee7320c5f6e5dff91d86560fe1a35bf98'
